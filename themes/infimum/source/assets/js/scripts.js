606bab3fc9d7fbac13290166125b837764b5df64
goodhoko <goodhoko@gmail.com>
2018-05-31 00:27:41 +0200

    Fixing problems with playpause buttons

--- themes/infimum/source/assets/js/scripts.js
+++ themes/infimum/source/assets/js/scripts.js
@@ -2,16 +2,22 @@ $(document).ready(function() {
 	console.log('Welcome to my blog!');
 
 	//enable lightGallery
-	$("article").lightGallery({selector: '.gallery-item'});
+	$('article').lightGallery({selector: '.gallery-item'});
 
 	//support for play and pause overlay button on html5 videos
 	$('article .video-wrapper').click(function () {
-	if($(this).children("video").get(0).paused){
-		$(this).children("video").get(0).play();
-		$(this).children(".playpause").fadeOut();
-	}else{
-	   $(this).children("video").get(0).pause();
-		$(this).children(".playpause").fadeIn();
-	}
-});
+		if($(this).children('video').get(0).paused){
+			$(this).children('video').get(0).play();
+			$(this).children('.playpause').fadeOut();
+		}else{
+		   $(this).children('video').get(0).pause();
+			$(this).children('.playpause').fadeIn();
+		}
+	});
+	$('article .video-wrapper video').on('play', function () {
+		$(this).siblings('.playpause').fadeOut();
+	})
+	$('article .video-wrapper video').on('pause', function () {
+		$(this).siblings('.playpause').fadeIn();
+	})
 });
