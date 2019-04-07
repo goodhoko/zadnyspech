64ac85a5a646060e9c497e2a8813d031b9110d27
goodhoko <goodhoko@gmail.com>
2019-04-07 18:18:03 +0200

    add pagination to frontpage

--- themes/infimum/source/assets/js/scripts.js
+++ themes/infimum/source/assets/js/scripts.js
@@ -10,27 +10,8 @@ $(document).ready(function() {
 
 
 
-	//support for play and pause overlay button on html5 videos
-	$('article .video-wrapper').click(function () {
-		if($(this).children('video').get(0).paused){
-			$(this).children('video').get(0).play();
-			$(this).children('.playpause').fadeOut();
-		}else{
-		   $(this).children('video').get(0).pause();
-			$(this).children('.playpause').fadeIn();
-		}
-	});
-	$('article .video-wrapper video').on('play', function () {
-		$(this).siblings('.playpause').fadeOut();
-	})
-	$('article .video-wrapper video').on('pause', function () {
-		$(this).siblings('.playpause').fadeIn();
-	})
-
-
-
 	//choosing hrandom header image
-	if(location.pathname == '/' || location.pathname.split('/')[1] == 'tags'){
+	if(location.pathname == '/' || location.pathname.split('/')[1] == 'tags' || location.pathname.split('/')[1] == 'minulost'){
 		var headers = $('.image-header').data('imgs')
 		var img = headers[Math.floor(Math.random() * headers.length)];
 		$('div.image-header').css('background-image', 'url(/' + encodeURI(img) + ')')
